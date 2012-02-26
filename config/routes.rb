71efdac13e5a138d81223f0eeadbc20bd2f0fbc3
vov4enso <vov4enso@gmail.com>
2012-02-26 16:38:56 +0200

    finished layouts and rotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Sample::Application.routes.draw do
+  get "users/new"
+
  match '/contact', :to => 'pages#contact'
  match '/help', :to => 'pages#help'
- match 'about', :tp => 'pages#about'
+ match '/about', :to => 'pages#about'
+ match 'signup', :to =>'users#new'
  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
